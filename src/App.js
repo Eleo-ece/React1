befaba78a8e466ad1d74dc39d41cbd4456430068
Eleo <rib_ele@outlook.fr>
2021-01-06 17:01:24 +0100

    new form submit

--- src/App.js
+++ src/App.js
@@ -1,38 +1,43 @@
-import React from "react";
-
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      counter: 0,
+      name : "" ,
+      
     };
+    this.handleChangeN=this.handleChangeN.bind(this)
   }
 
-  incrementCounter = () => {
-    this.setState{
-      counter: this.state.counter + 1,
-    })
-  }
-  render() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{this.state.counter}</p>
-        <button 
-        onClick={this.incrementCounter}
-        >
-          Click me
-        </button>
-      </header>
-    </div>
-  );
 
-  }
+render() {
   
+  const handleSubmit = event => {
+    event.preventDefault();
+    alert(this.state.name + ' : You have submitted the form.')
+  }
+  return(
+    <div className="wrapper">
+      <h1>Hello !</h1>
+      <form onSubmit={handleSubmit}>
+      <fieldset>
+         <label>
+           <p>Name</p>
+           <input value={this.state.pseudo} onChange={this.handleChangeN}/>
+           <p>{this.state.name}</p>
+         </label>
+       </fieldset>
+       <button onClick={() => this.setState({ name: this.state.name})}type="submit">Submit</button>
+       {console.log(window.name)}
+      </form>
+    </div>
+    
+  );
 }
-
+handleChangeN(event){
+  this.setState({name: event.target.value});
+}
+}
 export default App;
